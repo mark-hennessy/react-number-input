4682a79708c030cbc27d61b0ce2048efc47aec46
Mark Hennessy <markhennessy@deinestudienfinanzierung.de>
2020-06-17 14:40:00 +0200

    add labels above demo forms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.scss';
 import DemoForm from './DemoForm/DemoForm';
+import Label from './Label/Label';
 
 const CID = 'app';
 
 export default function App() {
   return (
     <div className={CID}>
+      <Label>Form 1, White Inputs</Label>
       <DemoForm formName='f1' blue />
+      <Label>Form 2, Blue Inputs, Duplicate Of Form 1 (no need to test)</Label>
       <DemoForm formName='f2' />
     </div>
   );
